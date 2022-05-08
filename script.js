bdc73cfb035eff5be15400f782389fd6fec84d51
lostted <lostted@protonmail.com>
2022-05-09 00:44:02 +0300

    Add a button to generate new grids

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 const pad = document.querySelector(".pad");
 
-for(let i = 0; i < 16; i++) {
-	let row = document.createElement("div");
-	row.classList.add("row");
-	for(let i = 0; i < 16; i++ ) {
-		let square = document.createElement("div");
-		row.appendChild(square);
+function generatePad(num = 16) {
 
-		square.addEventListener("mousemove", () => {
-			event.target.style.backgroundColor = "#09EBB0";
-		});
+	for(let i = 0; i < num; i++) {
+		let row = document.createElement("div");
+		row.classList.add("row");
+		for(let i = 0; i < num; i++ ) {
+			let square = document.createElement("div");
+			row.appendChild(square);
+	
+			square.addEventListener("mousemove", () => {
+				event.target.style.backgroundColor = "#09EBB0";
+			});
+		}
+		pad.appendChild(row);
 	}
-	pad.appendChild(row);
 }
+
+generatePad();
+
+const newGrid = document.querySelector(".newGrid");
+newGrid.addEventListener("click", () => {
+	while (pad.firstElementChild) {
+		pad.firstElementChild.remove();
+	}
+	let size = prompt("What size the new grid should be?");
+	if (isNaN(size) || size > 100 || size < 2) size = 16;
+	generatePad(size);
+});
