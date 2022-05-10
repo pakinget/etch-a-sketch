95003843514bf0387be148a171c14575c97fa8af
lostted <lostted@protonmail.com>
2022-05-10 03:15:54 +0300

    Add rainbow and gray shades modes

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const pad = document.querySelector(".pad");
 const colour = document.querySelector(".colour");
+const modes = document.querySelectorAll(".modeButton");
+let mode;
 
 function generatePad(num = 16) {
-
 	for(let i = 0; i < num; i++) {
 		let row = document.createElement("div");
 		row.classList.add("row");
@@ -10,15 +11,47 @@ function generatePad(num = 16) {
 			let square = document.createElement("div");
 			row.appendChild(square);
 	
-			square.addEventListener("mousemove", () => {
-				event.target.style.backgroundColor = colour.value;
+			square.addEventListener("mouseover", (e) => {
+				if (mode == "normal") e.target.style.backgroundColor = colour.value;
+				else if (mode == "rainbow") e.target.style.backgroundColor = `rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, 
+				${Math.floor(Math.random()*255)})`;
+				else if (mode == "gray") {
+					if (e.target.style.backgroundColor == "") e.target.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
+					else {
+						let currentArr = e.target.style.backgroundColor.split(",");
+						console.log(currentArr);
+						let current = currentArr[3].slice(1, 4);
+						console.log(current);
+						let added = `${Number(current)+0.1}`;
+						console.log(added);
+						e.target.style.backgroundColor = `rgba(0, 0, 0, ${added})`;
+						console.log(`rgba(0, 0, 0, ${added})`);
+					}
+				}
 			});
 		}
 		pad.appendChild(row);
 	}
 }
 
+modes.forEach((button) => {
+	button.addEventListener("click", () => {
+		changeMode(button.textContent);
+	});
+});
+
+function changeMode(newMode = "normal") {
+	modes.forEach((button) => {
+		if (button.textContent == newMode) {
+			button.classList.add("selected");
+			mode = button.textContent;
+		}
+		else button.classList.remove("selected");
+	});
+}
+
 generatePad();
+changeMode();
 
 const newGrid = document.querySelector(".newGrid");
 newGrid.addEventListener("click", () => {
