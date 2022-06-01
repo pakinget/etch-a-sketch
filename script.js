b428b7f286418cdeea7bc603e62f2221e2815c9a
pakinget <lostted@protonmail.com>
2022-06-01 19:38:09 +0300

    Remove console.log's from mouseover event

--- script.js
+++ script.js
@@ -14,29 +14,27 @@ body.addEventListener("mouseup", () => {
 });
 
 function generatePad(num = 16) {
-	for(let i = 0; i < num; i++) {
+	for (let i = 0; i < num; i++) {
 		let row = document.createElement("div");
 		row.classList.add("row");
-		for(let i = 0; i < num; i++ ) {
+		for (let i = 0; i < num; i++) {
 			let square = document.createElement("div");
 			row.appendChild(square);
-	
+
 			square.addEventListener("mouseover", (e) => {
 				if (!mouseDown) return;
-				else if (mode == "normal") e.target.style.backgroundColor = colour.value;
-				else if (mode == "rainbow") e.target.style.backgroundColor = `rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, 
-				${Math.floor(Math.random()*255)})`;
+				else if (mode == "normal")
+					e.target.style.backgroundColor = colour.value;
+				else if (mode == "rainbow")
+					e.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
 				else if (mode == "gray") {
-					if (e.target.style.backgroundColor == "") e.target.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
+					if (e.target.style.backgroundColor == "")
+						e.target.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
 					else {
-						let currentArr = e.target.style.backgroundColor.split(",");
-						console.log(currentArr);
+						let currentArr = e.target.style.backgroundColor.split(",");;
 						let current = currentArr[3].slice(1, 4);
-						console.log(current);
-						let added = `${Number(current)+0.1}`;
-						console.log(added);
+						let added = `${Number(current) + 0.1}`;
 						e.target.style.backgroundColor = `rgba(0, 0, 0, ${added})`;
-						console.log(`rgba(0, 0, 0, ${added})`);
 					}
 				}
 			});
@@ -56,8 +54,7 @@ function changeMode(newMode = "normal") {
 		if (button.textContent == newMode) {
 			button.classList.add("selected");
 			mode = button.textContent;
-		}
-		else button.classList.remove("selected");
+		} else button.classList.remove("selected");
 	});
 }
 
